269f878113a13d6e8691ab82d211fbd5feeabeb7
Lachkar Ahmed Salim <112639221+Lachkar-Ahmed-Salim@users.noreply.github.com>
2024-05-06 18:09:50 +0100

    Delete ollama_emb.py

--- ollama_emb.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.embeddings import OllamaEmbeddings
-embeddings = OllamaEmbeddings(model="nomic-embed-text")
-text = "This is a test document."
-query_result = embeddings.embed_query(text)
-msg=query_result[:5]
-print(msg)
